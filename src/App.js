90ef3b01a0ccb080e6f38093b14b8403d28576fd
pranay-j <57652119+pranay-j@users.noreply.github.com>
2022-02-11 14:54:54 +0530

    implemented meetups project on java script

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Route, Routes} from 'react-router-dom';
+import NewMeetupsPage from './pages/NewMeetups';
+import FavouritesPage from './pages/Favourites';
+import AllMeetupsPage from './pages/AllMeetups';
+import Layout from './components/layouts/Layout';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+    <Routes>
+      <Route path = '/' element={<AllMeetupsPage/>}> </Route>
+      <Route path = '/new-meetups' element={<NewMeetupsPage/>}></Route>
+      <Route path = '/favourites' element={<FavouritesPage/>}></Route>
+    </Routes>
+    </Layout>
   );
 }
 
